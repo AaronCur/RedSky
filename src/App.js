556005c27a1ad24c7a0b71e2210ddef3874c2af9
AaronCur <C00202505@itcarlow.ie>
2018-07-29 11:36:36 +0100

    Updated fetch to api to make it secure on github pages

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component{
       const country = e.target.elements.country.value;
 
       //Makes a template string allowing you to inject variables you have defined in your files
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
       //Converts response to JSON format
       const data = await api_call.json();
       if(city && country){
