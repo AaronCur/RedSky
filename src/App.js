8ea513c021b0e63c05b672fb271f0c3ea167b624
AaronCur <C00202505@itcarlow.ie>
2018-07-29 14:27:48 +0100

    Added error catch if the location entered is not valid

--- src/App.js
+++ src/App.js
@@ -23,28 +23,44 @@ class App extends React.Component{
       const country = e.target.elements.country.value;
 
       //Makes a template string allowing you to inject variables you have defined in your files
-      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-      //Converts response to JSON format
-      const data = await api_call.json();
-      if(city && country){
+      try{
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        //Converts response to JSON format
+        const data = await api_call.json();
+
+        if(city && country){
+          this.setState({
+             temperature: data.main.temp,
+             city: data.name,
+             country: data.sys.country,
+             humidity: data.main.humidity,
+             description: data.weather[0].description,
+             error: ""
+           });
+         }else {
+           this.setState({
+             temperature: undefined,
+             city: undefined,
+             country: undefined,
+             humidity: undefined,
+             description: undefined,
+             error: "Please enter the values."
+           });
+         }
+      }
+      //If api call fails display this error
+      catch(error){
         this.setState({
-           temperature: data.main.temp,
-           city: data.name,
-           country: data.sys.country,
-           humidity: data.main.humidity,
-           description: data.weather[0].description,
-           error: ""
-         });
-    }else {
-      this.setState({
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: "Please enter the values."
-      });
-    }
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "Please enter a valid location."
+        });
+      }
+
+
 
   }
 
